937f077e15d25331d8354ec8691dd6e455e6f21e
Zaky Thariq H <zakythariq@gmail.com>
2020-05-09 18:34:16 +0700

    bismillah

--- src/components/views/dosen.vue
+++ src/components/views/dosen.vue
@@ -54,7 +54,7 @@ export default {
     this.webdatarocks = new WebDataRocks({
       container: this.$el,
       toolbar: true,
-      height: 500,
+      height: 600,
       report: {
         dataSource: {
           dataSourceType: 'json',
